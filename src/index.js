c194a29d3a3695ae498941d968ee6a17b5782826
alvigabriel <claudio.alves20112011@hotmail.com>
2022-12-27 12:16:11 -0300

    alterando bd para elephantsql, refatorando database

--- src/index.js
+++ src/index.js
@@ -13,11 +13,7 @@ var cors = require('cors')
 const app = Express()
 app.use(cors())
 
-const host = process.env.DB_HOST
-const DBport = process.env.DB_PORT 
-const name = process.env.DB_NAME 
-const user = process.env.DB_USER 
-const password = process.env.DB_PASSWORD
+
 
 const port = process.env.DB_PORT || 5000
 
