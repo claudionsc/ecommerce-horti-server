5ed6ed53708fe27ef69c78c76a2df0961c646fa9
Claudio Nascimento <claudio.alves20112011@hotmail.com>
2023-05-18 23:57:45 -0300

    proibindo dados nulos

--- src/models/FrutasModels.js
+++ src/models/FrutasModels.js
@@ -5,23 +5,28 @@ const sequelize = require ('../config/_database')
 const FrutasModels = sequelize.define('Frutas', {
     genero: {
         type: Sequelize.STRING,
+         allowNull: false,
         required: true
     },
     nome: {
         type: Sequelize.STRING,
+         allowNull: false,
         required: true,
         unique: true
     },
     familia: {
         type: Sequelize.STRING,
+         allowNull: false,
         required: true
     },
     ordem: {
         type: Sequelize.STRING,
+         allowNull: false,
         required: true
     },
     imagem: {
         type: Sequelize.STRING(5000),
+         allowNull: false,
         required: true
     }
 })
